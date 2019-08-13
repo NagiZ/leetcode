bf5b411b4975b42c18490625f77c5a353a9517ea
NagiZ <863984461@qq.com>
2019-08-13 16:47:08 +0800

    Update 53.Maximum Subarray.js

--- exercises/53.Maximum Subarray.js
+++ exercises/53.Maximum Subarray.js
@@ -17,4 +17,43 @@ var maxSubArray = function(nums) {
     return ans
 };
 
-//
+//分治法
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    function _crossingMax(arr, start, mid, end) {
+		let _left = -Infinity,
+			_right = -Infinity,
+			sum = 0
+		for (let i = mid; i >= start; i--) {
+			sum += arr[i]
+			_left = Math.max(_left, sum)
+		}
+		sum = 0
+		for (let i = mid + 1; i <= end; i++) {
+			sum += arr[i]
+			_right = Math.max(_right, sum)
+		}
+		return _left + _right
+	}
+    function _maxSubArray(arr, start, end) {
+    	if (start === end) {
+    		return arr[end]
+    	} else {
+    		let mid = Math.floor((start + end) / 2),
+    			max_left = _maxSubArray(arr, start, mid),
+    			max_right = _maxSubArray(arr, mid + 1, end),
+    			max_crossing = _crossingMax(arr, start, mid, end)
+    		if (max_left >= max_right && max_left >= max_crossing) {
+    			return max_left
+    		} else if (max_right >= max_left && max_right >= max_crossing) {
+    			return max_right
+    		} else {
+    			return max_crossing
+    		}
+    	}
+    }
+    return _maxSubArray(nums, 0, nums.length - 1)
+};
