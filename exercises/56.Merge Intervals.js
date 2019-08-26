8f57612d4de21cde43a2593c57ce25c59c3228ad
NagiZ <863984461@qq.com>
2019-08-26 16:14:09 +0800

    Create 56.Merge Intervals.js

---
+++ exercises/56.Merge Intervals.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+//先排序在逐个合并
+var insert = function(intervals, newInterval) {
+	var _list = [];
+    for (var i = 0; i < intervals.length; i++) {
+    	_list.push(intervals[i][0], intervals[i][1])
+    }
+    if (!_list.length) return [newInterval]
+    var s = 0,
+		e = _list.length - 1,
+		fs = false,
+		fe = false;
+	while(!(fs && fe)) {
+		if (_list[s] >= newInterval[0]) {
+			fs = true		
+		} else {
+			s++
+		}
+		if (_list[e] <= newInterval[1]) {
+			fe = true
+		} else {
+			e--
+		}
+	}
+	e = e + 1
+	var l = e - s
+	if (!(e % 2)) {
+		_list.splice(e, 0, newInterval[1])
+	}
+	if (!(s % 2)) {
+		_list.splice(s, 0, newInterval[0])
+		s++
+		l--
+	}
+	_list.splice(s, l)
+	var result = []
+	for (var i = 0; i < _list.length; i = i + 2) {
+		result.push([_list[i], _list[i + 1]])
+	}
+	return result
+};
