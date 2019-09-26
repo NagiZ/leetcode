c74791eba5df7468bd3d1a96293b035fd4ff6210
NagiZ <863984461@qq.com>
2019-09-26 11:59:18 +0800

    Create 5197.Ugly NumberIII.js

---
+++ exercises/5197.Ugly NumberIII.js
@@ -0,0 +1,39 @@
+var minimumAbsDifference = function(arr) {
+    function quickSort(a1){
+        if (!a1) {
+            return;
+        }
+        if(a1.length<=1){return a1;}
+        var arr = a1.slice(0);
+        var pivotIndex=Math.floor(arr.length/2);
+        var pivot=arr.splice(pivotIndex,1)[0];
+        var left=[];
+        var right=[];
+        for(var i=0;i<arr.length;i++){
+            if(arr[i] <= pivot){
+                left.push(arr[i]);
+            }
+            else{
+                right.push(arr[i]);
+            }
+        }
+        return quickSort(left).concat([pivot],quickSort(right));
+    }
+    arr = quickSort(arr)
+    console.log(arr)
+    var i = 0,
+        ans = [],
+        min = Infinity
+    for (var i = 0; i< arr.length - 1; i++) {
+        var temp = arr[i+1] - arr[i]
+        if (min < temp) {
+            continue
+        }
+        if (min > temp) {
+            min = temp
+            ans = []
+        }
+        ans.push([arr[i], arr[i+1]])
+    }
+    return ans
+};
