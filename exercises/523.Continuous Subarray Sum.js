bb2ef3a77a85088291e3c278108b0100bcd3351c
NagiZ <863984461@qq.com>
2019-09-29 09:55:26 +0800

    Update 523.Continuous Subarray Sum.js

--- exercises/523.Continuous Subarray Sum.js
+++ exercises/523.Continuous Subarray Sum.js
@@ -19,3 +19,28 @@ var checkSubarraySum = function(nums, k) {
     }
     return false
 };
+
+/*solution 2 来自官方题解
+* 用haspMap保存到第i个位置为止的累积和，并将这个前缀和对k取余rem。当rem已经存在与haspMap里，则说明存在以i、j为始终端点的数组，其数组和为k的整数倍。
+*/
+var checkSubarraySum2 = function(nums, k) {
+    var map = {},
+        sum = 0
+    for (var i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        if (k != 0) {
+            sum %= k
+        }
+        if (i > 0 && sum === 0) return true
+        if (map[sum] != undefined) {
+            if (i - map[sum] >= 1) {
+                return true
+            }
+        } else {
+            if (nums[i] != 0) {
+                map[sum] = i + 1
+            }
+        }
+    }
+    return false
+};
