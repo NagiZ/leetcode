9156266d38cd06eb310ec1a14322f74a403a28fb
NagiZ <863984461@qq.com>
2021-01-15 11:49:04 +0800

    Update 56.Merge Intervals.js

--- exercises/56.Merge Intervals.js
+++ exercises/56.Merge Intervals.js
@@ -43,3 +43,24 @@ var insert = function(intervals, newInterval) {
 	}
 	return result
 };
+
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+	intervals.sort((a, b) => {
+        return a[0] - b[0]
+    })
+	var i = 1
+	while(i < intervals.length) {
+		if (intervals[i][0] <= intervals[i-1][1]) {
+			intervals[i-1][1] = Math.max(intervals[i-1][1], intervals[i][1])
+			intervals.splice(i, 1)
+		} else {
+			i++
+		}
+	}
+	return intervals
+};
